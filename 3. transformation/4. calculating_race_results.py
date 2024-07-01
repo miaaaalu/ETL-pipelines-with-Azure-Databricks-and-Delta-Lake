dbe8649bf1f73b2fb6c1fe3db65e8bfaf5f05459
miaaaalu <miaaaalu>
2024-07-01 09:58:09 +0000

    update

--- 3. transformation/4. calculating_race_results.py
+++ 3. transformation/4. calculating_race_results.py
@@ -4,29 +4,27 @@ v_file_date = dbutils.widgets.get("p_file_date")
 
 # COMMAND ----------
 
-# MAGIC %sql
-# MAGIC create or replace temp view race_result_upd
-# MAGIC as 
-# MAGIC select 
-# MAGIC   rc.race_year
-# MAGIC   , c.name as team_name
-# MAGIC   , d.driver_id
-# MAGIC   , d.name as driver_name
-# MAGIC   , rc.race_id
-# MAGIC   , rs.position
-# MAGIC   , rs.points
-# MAGIC   , 11 - rs.position as calculated_points
-# MAGIC from staging.results rs 
-# MAGIC join staging.drivers d      on d.driver_id = rs.driver_id
-# MAGIC join staging.constructors c on c.constructor_id = rs.constructor_id
-# MAGIC join staging.races rc       on rs.race_id = rc.race_id
-# MAGIC where 
-# MAGIC   rs.position <= 10
-# MAGIC   and rs.file_date = '{v_file_date}'
+# DBTITLE 1,1. CREATE TABLE WITH SCHEMA
+spark.sql(f"""
+  create table if not exists presentation.calculated_race_results
+  (
+    race_year int,
+    team_name string, 
+    driver_id int,
+    driver_name string,
+    race_id int, 
+    position int,
+    points int,
+    calculated_points int,
+    created_date timestamp,
+    updated_date timestamp
+  )
+  using delta
+""")
 
 # COMMAND ----------
 
-# DBTITLE 1,1. create temp view
+# DBTITLE 1,2. CREATE TEMP VIEW
 spark.sql(f"""
     create or replace temp view race_result_upd
     as 
@@ -40,6 +38,7 @@ spark.sql(f"""
         , rs.points
         , 11 - rs.position as calculated_points
     from staging.results rs 
+    -- FULL LOAD TABLES  
     join staging.drivers d      on d.driver_id = rs.driver_id
     join staging.constructors c on c.constructor_id = rs.constructor_id
     join staging.races rc       on rs.race_id = rc.race_id
@@ -50,39 +49,25 @@ spark.sql(f"""
 
 # COMMAND ----------
 
+# DBTITLE 1,3. CREATE MERGE STATEMENT
 spark.sql(f"""
-  create table if not exists presentation.calculated_race_results
-  (
-    race_year int,
-    team_name string, 
-    driver_id int,
-    driver_name string,
-    race_id int, 
-    position int,
-    calculated_points int,
-    created_date timestamp,
-    updated_date timestamp
-  )
-  using delta
+          
+    MERGE INTO presentation.calculated_race_results as tgt
+    USING race_result_upd as upd
+    ON (tgt.driver_id = upd.driver_id and tgt.race_id = upd.race_id)
+    WHEN MATCHED THEN
+    UPDATE SET tgt.position = upd.position,
+                tgt.points = upd.points,
+                tgt.calculated_points = upd.calculated_points,
+                tgt.updated_date = current_timestamp
+    WHEN NOT MATCHED
+    THEN INSERT (race_year, team_name, driver_id, driver_name, race_id, position, calculated_points,created_date ) 
+        VALUES (race_year, team_name, driver_id, driver_name, race_id, position, calculated_points, current_timestamp)        
 """)
 
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC MERGE INTO f1_demo.drivers_merge tgt
-# MAGIC USING drivers_day1 upd
-# MAGIC ON tgt.driverId = upd.driverId
-# MAGIC WHEN MATCHED THEN
-# MAGIC   UPDATE SET tgt.dob = upd.dob,
-# MAGIC              tgt.forename = upd.forename,
-# MAGIC              tgt.surname = upd.surname,
-# MAGIC              tgt.updatedDate = current_timestamp
-# MAGIC WHEN NOT MATCHED
-# MAGIC   THEN INSERT (driverId, dob, forename,surname,createdDate ) VALUES (driverId, dob, forename,surname, current_timestamp)
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC select * 
-# MAGIC from presentation.calculated_race_results
-# MAGIC
+# MAGIC -- drop table presentation.calculated_race_results;
+# MAGIC select count(*) from presentation.calculated_race_results;
